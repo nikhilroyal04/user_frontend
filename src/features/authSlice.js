4986b8621f6e3a826ab895046c6bf62342a677b8
NikhilRoyal04 <nikhilchaudhary390@gmail.com>
2024-10-17 18:31:31 +0530

    update

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -4,7 +4,6 @@ import axios from "axios";
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-    user: JSON.parse(sessionStorage.getItem("user")) || null,
     isLoading: false,
     error: null,
     token: sessionStorage.getItem("token") || null,
@@ -14,12 +13,10 @@ const authSlice = createSlice({
       state.isLoading = true;
       state.error = null;
     },
-    setUser: (state, action) => {
+    setToken: (state, action) => {
       state.isLoading = false;
-      state.user = action.payload.user;
       state.token = action.payload.token;
 
-      sessionStorage.setItem("user", JSON.stringify(action.payload.user));
       sessionStorage.setItem("token", action.payload.token);
     },
     setError: (state, action) => {
@@ -30,22 +27,19 @@ const authSlice = createSlice({
       state.error = null;
     },
     logout: (state) => {
-      state.user = null;
       state.token = null;
-
-      sessionStorage.removeItem("user");
       sessionStorage.removeItem("token");
     },
   },
 });
 
-export const { setLoading, setUser, setError, clearError, logout } = authSlice.actions;
+export const { setLoading, setToken, setError, clearError, logout } = authSlice.actions;
 
 export const loginUser = (credentials) => async (dispatch) => {
   dispatch(setLoading());
   try {
     const response = await axios.post(import.meta.env.VITE_BASE_URL + "auth/login", credentials);
-    dispatch(setUser(response.data.data));
+    dispatch(setToken(response.data.data)); // Adjusted to only set token
   } catch (error) {
     dispatch(setError(error.response?.data?.message || "Login failed"));
   }
@@ -55,7 +49,7 @@ export const logoutUser = () => (dispatch) => {
   dispatch(logout());
 };
 
-export const selectAuthUser = (state) => state.auth.user;
+export const selectAuthToken = (state) => state.auth.token; // Adjusted to select token
 export const selectAuthLoading = (state) => state.auth.isLoading;
 export const selectAuthError = (state) => state.auth.error;
 
